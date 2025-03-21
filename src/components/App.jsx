5f3b035c1bdfe23d77ede4fc6caf837915e23543
GratielaC <gratiela.chirtop@yahoo.com>
2025-03-21 15:44:06 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,72 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import SearchBar from './searchBar/SearchBar';
+import ImageGallery from './imageGallery/ImageGallery';
+import Button from './button/Button';
+import { fetchImages } from 'services/imagesApi';
+import styles from './App.module.css';
+import Loader from './loader/Loader';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+      error: null,
+      query: '',
+      page: 1,
+      isLoading: false,
+      hasMoreImages: true,
+    };
+  }
+
+  onSubmit = query => {
+    this.setState({ query, page: 1, images: [], isLoading: true }, () => {
+      this.loadImages(query, 1);
+    });
+  };
+
+  loadImages = async (query, page) => {
+    try {
+      const data = await fetchImages(query, page); 
+
+      this.setState(prevState => {
+        const newImages = [...prevState.images, ...data.hits];
+        const totalImages = data.totalHits;
+        const hasMore = newImages.length < totalImages && data.hits.length > 0;
+
+        return {
+          images: newImages,
+          isLoading: false,
+          hasMoreImages: hasMore,
+        };
+      });
+    } catch (error) {
+      this.setState({ error: error.message, isLoading: false });
+      console.error('Fetch error:', error);
+    }
+  };
+
+  
+  handleLoadMore = () => {
+    this.setState(
+      prevState => ({ page: prevState.page + 1, isLoading: true }),
+      () => {
+        this.loadImages(this.state.query, this.state.page);
+      }
+    );
+  };
+
+  render() {
+    const { query, images, isLoading, hasMoreImages } = this.state;
+    return (
+      <div className={styles.app}>
+        <SearchBar onSubmit={this.onSubmit} />
+        <ImageGallery images={this.state.images} />
+        {query && images.length > 0 && hasMoreImages && !isLoading && (
+          <Button onClick={this.handleLoadMore} />
+        )}
+        {query && isLoading && <Loader visible={isLoading} />}
+      </div>
+    );
+  }
+}
